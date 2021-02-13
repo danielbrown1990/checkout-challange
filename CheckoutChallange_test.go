b53de2dd4ce2d66c3927d309a52c6b0cba865c77
Dan <dan@pi-net.lan>
2021-02-13 14:31:21 +0000

    Adding initial test cases

--- CheckoutChallange_test.go
+++ CheckoutChallange_test.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"checkout-challange/checkout"
+	"testing"
+)
+
+func Test_SingleItems(t *testing.T) {
+	//Arrange
+	tests := []struct {
+		SKU  string
+		want int
+	}{
+		{"A", 50},
+		{"B", 30},
+		{"C", 20},
+		{"D", 15},
+	}
+
+	for _, test := range tests {
+		//Act
+		sut := checkout.NewCheckout()
+		sut.Scan(test.SKU)
+		got := sut.Total()
+
+		//Assert
+		if test.want != got {
+			t.Fatalf("incorrect total want(%v) have (%v)", test.want, got)
+		}
+	}
+}
+
+func Test_MultipleItems(t *testing.T) {
+	//Arrange
+	tests := []struct {
+		SKUs []string
+		want int
+	}{
+		{[]string{"A", "B", "C", "D"}, 115},
+	}
+
+	for _, test := range tests {
+		//Act
+		sut := checkout.NewCheckout()
+		for _, SKU := range test.SKUs {
+			sut.Scan(SKU)
+		}
+
+		got := sut.Total()
+
+		//Assert
+		if test.want != got {
+			t.Fatalf("incorrect total want(%v) have (%v)", test.want, got)
+		}
+	}
+}
