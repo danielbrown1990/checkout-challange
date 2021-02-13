3e8c1314ba48c2c27eef58032a207fb7f015366c
Dan <dan@pi-net.lan>
2021-02-13 14:18:57 +0000

    Adding scaffolded checkout implementation

--- checkout/checkout.go
+++ checkout/checkout.go
@@ -6,3 +6,19 @@ type Checkout interface {
 	Scan(SKU string)
 	Total() int
 }
+
+type checkout struct{}
+
+// NewCheckout is a constructor for the
+// default implementation of Checkout
+func NewCheckout() Checkout {
+	return &checkout{}
+}
+
+func (c *checkout) Scan(SKU string) {
+
+}
+
+func (c *checkout) Total() int {
+	return 0
+}
