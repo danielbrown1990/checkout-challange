882bd00571202af2a8630d247e7fa5eb2796f469
Dan <dan@pi-net.lan>
2021-02-13 16:10:34 +0000

    Implementing command line example

--- CheckoutChallange.go
+++ CheckoutChallange.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"checkout-challange/catalogue"
+	"checkout-challange/checkout"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
 
+var (
+	catalogueLocation = "cmd/testdata/catalogue.json"
+)
+
+func main() {
+	flag.StringVar(&catalogueLocation, "catalogue_location", catalogueLocation, "flag to override default catalogue location")
+	flag.Parse()
+
+	cat, err := catalogue.NewCatalogue(catalogueLocation)
+	if err != nil {
+		panic(fmt.Errorf("Error reading catalogue configuration at %v", catalogueLocation))
+	}
+
+	check := checkout.NewCheckout(cat)
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("Enter SKU or type R to reset\n current total: %v\n>", check.Total())
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			panic(fmt.Errorf("Error reading user input %v", err))
+		}
+		SKU := strings.ReplaceAll(input, "\n", "")
+
+		if SKU == "R" {
+			check = checkout.NewCheckout(cat)
+			continue
+		}
+
+		err = check.Scan(SKU)
+		if err != nil {
+			fmt.Printf("Unknown SKU entered: %v\nenter SKU type R to reset\n current total: %v\n>", SKU, check.Total())
+		}
+
+	}
 }
