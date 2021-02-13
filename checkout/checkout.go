78797908c44058ad90a8e43b9af5cc3fd7205cef
Dan <dan@pi-net.lan>
2021-02-13 14:36:01 +0000

    Implementing scan method

--- checkout/checkout.go
+++ checkout/checkout.go
@@ -7,16 +7,27 @@ type Checkout interface {
 	Total() int
 }
 
-type checkout struct{}
+type checkout struct {
+	items map[string]int
+}
 
 // NewCheckout is a constructor for the
 // default implementation of Checkout
 func NewCheckout() Checkout {
-	return &checkout{}
+	return &checkout{
+		items: make(map[string]int),
+	}
 }
 
 func (c *checkout) Scan(SKU string) {
+	count, ok := c.items[SKU]
+	if !ok {
+		c.items[SKU] = 1
+		return
+	}
 
+	count++
+	c.items[SKU] = count
 }
 
 func (c *checkout) Total() int {
